7d7669b6441c5b4216b2a5dac72cbdfed9fda42b
Alexis <colo1990.ag@gmail.com>
2025-04-21 18:34:32 -0300

    feat: enhance AddressPage and AddressForm to manage user addresses with login validation and address retrieval

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,17 +1,35 @@
 import { Title } from '@/components/title/Title';
 import { AddressForm } from './ui/AddressForm';
 import { getCountries } from '@/actions/country/get-countries';
+import { auth } from '@/auth.config';
+import { getUserAddress } from '@/actions/address/get-user-address';
 
 export default async function AddressPage() {
 
   const countries = await getCountries()
+
+  const session = await auth()
+
+  if (!session?.user) {
+    return (
+      <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
+        <div className="w-full xl:w-[1000px] flex flex-col justify-center text-left">
+          <Title title="Address" subtitle="Delivery address" />
+
+          <p className='text-red-500'>Please login to see your address</p>
+        </div>
+      </div>
+    )
+  }
+
+  const userAddress = await getUserAddress(session.user.id) ?? undefined
 
   return (
     <div className="flex flex-col sm:justify-center sm:items-center mb-72 px-10 sm:px-0">
       <div className="w-full xl:w-[1000px] flex flex-col justify-center text-left">
         <Title title="Address" subtitle="Delivery address" />
 
-        <AddressForm countries={countries} />
+        <AddressForm countries={countries} userStoredAddress={userAddress} />
       </div>
     </div>
   );
